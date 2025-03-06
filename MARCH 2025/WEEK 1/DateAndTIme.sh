065057ce86b59def2f267a606c2c38d84d2c8af4
fazilmca <fazilmca1@gmail.com>
2025-03-06 11:04:49 +0530

    Update DateAndTIme.sh

--- MARCH 2025/WEEK 1/DateAndTIme.sh
+++ MARCH 2025/WEEK 1/DateAndTIme.sh
@@ -11,3 +11,12 @@ echo "Current Date: $current_date"
 echo "Current Time: $current_time"
 echo "Username: $username"
 echo "Current Working Directory: $current_directory"
+
+
+
+-----------------------------
+output:
+-----------------------------
+Current Date: 2025-03-06
+Current Time: 10:42:50
+Username: mca
